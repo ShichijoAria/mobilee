34818d43be143a93b7b24e79c1da20b7dc7363dc
shichijoaria <Cww048175!>
2017-11-08 21:49:14 +0800

    增加登录日志

--- src/main/java/edu/xhu/mobilee/dao/AdminDao.java
+++ src/main/java/edu/xhu/mobilee/dao/AdminDao.java
@@ -2,9 +2,13 @@ package edu.xhu.mobilee.dao;
 
 import edu.xhu.mobilee.entity.AdminEntity;
 
+import java.util.Map;
+
 public interface AdminDao {
 
     AdminEntity findAdminById(long id);
 
     int updateAdminById(AdminEntity adminEntity);
+
+    Map<String,Object> CountTableRows();
 }
