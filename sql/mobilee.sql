92b616d6e0766a62ff758af3ec156ff6eca837ae
aria <15608176098@163.com>
2018-01-17 18:33:08 +0800

    sql语句

--- sql/mobilee.sql
+++ sql/mobilee.sql
@@ -1,17 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : mobilee
-Source Server Version : 50718
-Source Host           : localhost:3306
-Source Database       : mobilee
-
-Target Server Type    : MYSQL
-Target Server Version : 50718
-File Encoding         : 65001
-
-Date: 2017-11-16 15:08:53
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
