c25937a389bdaf0bb5126d134ba08d944e691776
aria <15608176098@163.com>
2018-01-18 15:11:43 +0800

    sql语句

---
+++ src/main/java/edu/xhu/mobilee/shiro/MyRealm.java
@@ -0,0 +1,30 @@
+package edu.xhu.mobilee.shiro;
+
+import edu.xhu.mobilee.service.AdminService;
+import edu.xhu.mobilee.service.UserService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MyRealm extends AuthorizingRealm {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AdminService adminService;
+
+    public static final String SESSION_USER_KEY = "secret";
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
